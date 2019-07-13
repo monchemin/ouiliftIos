dd6c71886ce290d6189da03c128c72d55a5dc613
ameadev <amewena@gmail.com>
2019-07-13 18:45:41 -0400

    Setup : OK Profile choice screen: OK

---
+++ ouilift/ouilift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ouilift
+//
+//  Created by Mewena on 2019-07-13.
+//  Copyright © 2019 AwessoMeTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
